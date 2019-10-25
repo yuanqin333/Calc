b9e06d0ec2d55fba65fb68b1e01ec7ccc261da07
kangs <kangs>
2019-10-24 21:46:13 -0700

    Initial commit

---
+++ src/main/java/com/kavinschool/calc/Calc.java
@@ -0,0 +1,27 @@
+package com.kavinschool.calc;
+
+public class Calc {
+	private double x, y;
+
+	Calc(double a, double b) {
+		x = a;
+		y = b;
+	}
+
+	public double add() {
+		return x + y;
+	}
+
+	public double mul() {
+		return x * y;
+	}
+
+	public double sub() {
+		return x - y;
+	}
+
+	public double div() {
+		return x / y;
+	}
+
+}
---
+++ src/main/java/com/kavinschool/calc/DoMath.java
@@ -0,0 +1,17 @@
+package com.kavinschool.calc;
+
+public class DoMath {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Calc myCalc = new Calc(8, 2);
+
+		System.out.println("Add: " + myCalc.add());
+		System.out.println("Mult: " + myCalc.mul());
+		System.out.println("Sub: " + myCalc.sub());
+		System.out.println("Div: " + myCalc.div());
+	}
+
+}
